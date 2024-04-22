3b4d3a12d4d2e3e4cbfc4a6085a15aacf1b565c1
Yagnesh Modh <contact@thespecialcharacter.com>
2024-04-22 09:47:02 +0530

    commit code

--- tailwind.js
+++ tailwind.js
@@ -23,82 +23,76 @@ tailwind.config = {
             fontWeight: "700",
           },
         ],
+  
         h2: [
-          "1.875rem",
+          "clamp(1.75rem, 1.563vw + 1.438rem, 2.188rem)",
           {
-            lineHeight: "2.25rem",
-            letterSpacing: "-0.02em",
+            lineheight: "clamp(2.275rem, 2.031vw + 1.869rem, 2.844rem)",
             fontWeight: "700",
           },
         ],
+  
         h3: [
-          "1.875rem",
+          "clamp(1.625rem, 0.893vw + 1.446rem, 1.875rem)",
           {
-            lineHeight: "2.25rem",
-            letterSpacing: "-0.02em",
+            lineHeight: " clamp(2.275rem, 1.25vw + 2.025rem, 2.625rem)",
             fontWeight: "700",
           },
         ],
+  
         h4: [
-          "1.875rem",
+          "clamp(1.5rem, 0.893vw + 1.321rem, 1.75rem)",
           {
-            lineHeight: "2.25rem",
-            letterSpacing: "-0.02em",
+            lineHeight: "clamp(2.1rem, 1.25vw + 1.85rem, 2.45rem)",
             fontWeight: "700",
           },
         ],
         h5: [
-          "1.875rem",
+          "clamp(1.375rem, 0.893vw + 1.196rem, 1.625rem)",
           {
-            lineHeight: "2.25rem",
-            letterSpacing: "-0.02em",
+            lineHeight: "clamp(2.063rem, 1.339vw + 1.795rem, 2.438rem)",
             fontWeight: "700",
           },
         ],
         h6: [
-          "1.875rem",
+          "clamp(1.25rem, 0.446vw + 1.161rem, 1.375rem)",
           {
-            lineHeight: "2.25rem",
-            letterSpacing: "-0.02em",
+            lineHeight: "clamp(1.875rem, 0.67vw + 1.741rem, 2.063rem)",
             fontWeight: "700",
           },
         ],
-        body: [
-          "1.875rem",
+        p: [
+          "clamp(1.063rem, 0vw + 1.063rem, 1.063rem)",
           {
-            lineHeight: "2.25rem",
-            letterSpacing: "-0.02em",
-            fontWeight: "700",
+            lineHeight: "clamp(1.594rem, 0vw + 1.594rem, 1.594rem)",
+            fontWeight: "400",
           },
         ],
         list: [
-          "1.875rem",
+          "clamp(1.063rem, 0.223vw + 1.018rem, 1.125rem)",
           {
-            lineHeight: "2.25rem",
-            letterSpacing: "-0.02em",
-            fontWeight: "700",
+            lineHeight: "clamp(1.594rem, 0.335vw + 1.527rem, 1.688rem)",
+            fontWeight: "400",
           },
         ],
         link: [
-          "1.875rem",
+          "clamp(1rem, 0.446vw + 0.911rem, 1.125rem)",
           {
-            lineHeight: "2.25rem",
-            letterSpacing: "-0.02em",
-            fontWeight: "700",
+            lineHeight: "clamp(1.5rem, 0.67vw + 1.366rem, 1.688rem)",
+            fontWeight: "400",
           },
         ],
         quote: [
-          "1.875rem",
+          "clamp(1.25rem, 0.446vw + 1.161rem, 1.375rem)",
           {
-            lineHeight: "2.25rem",
-            letterSpacing: "-0.02em",
-            fontWeight: "700",
+            lineHeight: "clamp(1.875rem, 0.67vw + 1.741rem, 2.063rem)",
+            fontWeight: "300",
           },
         ],
         button: [
-          "1.125rem",
+          "clamp(1.125rem, 0vw + 1.125rem, 1.125rem)",
           {
-            lineHeight: "1.6875rem",
+            lineHeight: "clamp(1.688rem, 0vw + 1.688rem, 1.688rem)",
             fontWeight: "500",
           },
         ],
