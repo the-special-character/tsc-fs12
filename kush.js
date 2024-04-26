558714f0f79f7c9028b2c4bcf41ba2a78f1780d0
Kush Vaishnav <kushvaishnav234@gmail.com>
2024-04-26 10:16:30 +0530

    updated

--- kush.js
+++ kush.js
@@ -1,5 +1,5 @@
 const firstName = "Kush";
 const lastName = "Vaishnav";
 const pass = "car";
-
-console.log(firstName +" "+ lastName  +"'s "+ pass + " is super cool" )
+console.log("My name is" + firstName + + " " + lastName);
+console.log(firstName + " " + lastName + "'s " + pass + " is super cool");
