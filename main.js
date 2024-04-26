f058450c165ace6a17d2e6fd5452505cb77b350b
Samyak Patel <110371889+samyak2092@users.noreply.github.com>
2024-04-26 10:06:24 +0530

    java code

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstName = "samyak"
+
+const lastName = "patel"
+
+const poss = "car"
+
+console.log("my name is " + firstName+ " " + lastName);
+console.log(firstName + " " + lastName + " "+ poss + " " + "is super cool")
