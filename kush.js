66a9c6ac8c85a548c26ce45b310a22ed8918ce33
Kush Vaishnav <kushvaishnav234@gmail.com>
2024-04-26 10:42:55 +0530

    Updated

--- kush.js
+++ kush.js
@@ -1,5 +1,53 @@
-const firstName = "Kush";
-const lastName = "Vaishnav";
-const pass = "car";
-console.log("My name is" + firstName + + " " + lastName);
-console.log(firstName + " " + lastName + "'s " + pass + " is super cool");
+// const firstName = "Kush";
+// const lastName = "Vaishnav";
+// const pass = "car";
+// console.log("My name is" + firstName + + " " + lastName);
+// console.log(firstName + " " + lastName + "'s " + pass + " is super cool");
+
+// // <!DOCTYPE html>
+// // <html lang="en">
+// // <head>
+// //     <meta charset="UTF-8">
+// //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+// //     <title>Document</title>
+// // </head>
+// // <body>
+    
+// // </body>
+// // </html>
+
+const str1 = "<!DOCTYPE html>";
+const str2 = "<html lang='en'>";
+const str3 = "<head>"
+const str4 = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0<meta charset='UTF-8'>"
+const str5 = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+const str6 = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0<title>Document</title>"
+const str7 = "</head>"
+const str8 = "<body>"
+const str9 = "</body>"
+const str10 = "</html>"
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(str5);
+console.log(str6);
+console.log(str7);
+console.log(str8);
+console.log(str9);
+console.log(str10);
+
+const htmlDocument = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+</body>
+</html>
+`;
+
+console.log(htmlDocument);
