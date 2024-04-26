da3ba7416c4fa38fd2b0ddc7cc894535fa8743bf
ThakkarNikunj <nikuthakkar12345@gmail.com>
2024-04-26 10:06:44 +0530

    added

---
+++ Untitled-2.js
@@ -0,0 +1,9 @@
+
+const firstname = "nikunj" ;
+
+const lastname = "thakkar";
+
+const pass ="car";
+
+    console.log( "my name is" + " "+firstname + " " + lastname);
+    console.log("i own" + " " + "a" +" "+ pass);
