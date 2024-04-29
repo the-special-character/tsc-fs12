a23bc0471be475d2183e7b173b1234559ec9da6a
Kush Vaishnav <kushvaishnav234@gmail.com>
2024-04-29 10:21:32 +0530

    string

--- kush.js
+++ kush.js
@@ -1,5 +1,14 @@
 // const firstName = "Kush";
 // const lastName = "Vaishnav";
+// const role = "Full Stack Developer";
+// const exp = "1 year";
+// const data = `hello, My name is ${firstName} ${lastName} and i am ${role} and i have total ${exp} of experience`;
+// console.log(data);
+
+
+
+// const firstName = "Kush";
+// const lastName = "Vaishnav";
 // const pass = "car";
 // console.log("My name is" + firstName + + " " + lastName);
 // console.log(firstName + " " + lastName + "'s " + pass + " is super cool");
@@ -16,38 +25,38 @@
 // // </body>
 // // </html>
 
-const str1 = "<!DOCTYPE html>";
-const str2 = "<html lang='en'>";
-const str3 = "<head>"
-const str4 = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0<meta charset='UTF-8'>"
-const str5 = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-const str6 = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0<title>Document</title>"
-const str7 = "</head>"
-const str8 = "<body>"
-const str9 = "</body>"
-const str10 = "</html>"
-console.log(str1);
-console.log(str2);
-console.log(str3);
-console.log(str4);
-console.log(str5);
-console.log(str6);
-console.log(str7);
-console.log(str8);
-console.log(str9);
-console.log(str10);
+// const str1 = "<!DOCTYPE html>";
+// const str2 = "<html lang='en'>";
+// const str3 = "<head>"
+// const str4 = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0<meta charset='UTF-8'>"
+// const str5 = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+// const str6 = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0<title>Document</title>"
+// const str7 = "</head>"
+// const str8 = "<body>"
+// const str9 = "</body>"
+// const str10 = "</html>"
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+// console.log(str4);
+// console.log(str5);
+// console.log(str6);
+// console.log(str7);
+// console.log(str8);
+// console.log(str9);
+// console.log(str10);
 
-const htmlDocument = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-</body>
-</html>
-`;
+// const htmlDocument = `
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+// </body>
+// </html>
+// `;
 
-console.log(htmlDocument);
+// console.log(htmlDocument);
