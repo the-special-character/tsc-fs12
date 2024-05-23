44ef5e39959911d79119314888e1e350cbd271c2
darshan <chotaliyadarshan710@gmail.com>
2024-05-23 12:28:42 +0530

    darshan

--- .vscode/tailwind.config.js
+++ .vscode/tailwind.config.js
@@ -1,115 +1,118 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./**/*.{html,js}"],
     theme: {
-        colors: {
-            primary: "#263238",
-            secondary: "#ff8c92",
-            box: "#f2f2f2",
-            content: "#f2f2f2",
-            pinkBcakground: "#fdf1fd",
-            whiteBackgrond: "#f2f2f2",
-            primrylightBackground: "#f3eab",
-            lightgrayBackground: "#f2f6f7",
+      colors: {
+        primary: "#263238",
+        secondary: "#ff8c92",
+        box: "#f2f2f2",
+        content: "#4e4e4e",
+        pinkBackground: "#fdf1fd",
+        whiteBackground: "#f2f2f2",
+        primaryLightBackground: "#f3eaeb",
+        lightGrayBackground: "#f2f6f7",
+        white: "#fff",
+      },
+      fontSize: {
+        h1: [
+          "2.5rem",
+          {
+            lineHeight: "2rem",
+            letterSpacing: "-0.01em",
+            fontWeight: "500",
           },
-          fontFamily: [
-            {
-              title: "open-sans, sans-serif",
-              body: "Inter, sans-serif",
-            },
-          ],
-          fontSize: {
-            fontSize: {
-              h1: [
-                "2.5rem",
-                {
-                  lineHeight: "2rem",
-                  letterSpacing: "-0.01em",
-                  fontWeight: "500",
-                },
-              ],
-              h2: [
-                "1.875rem",
-                {
-                  lineHeight: "2.25rem",
-                  letterSpacing: "-0.02em",
-                  fontWeight: "700",
-                },
-              ],
-              h3: [
-                "1.875rem",
-                {
-                  lineHeight: "2.25rem",
-                  letterSpacing: "-0.02em",
-                  fontWeight: "700",
-                },
-              ],
-              h4: [
-                "1.875rem",
-                {
-                  lineHeight: "2.25rem",
-                  letterSpacing: "-0.02em",
-                  fontWeight: "700",
-                },
-              ],
-              h5: [
-                "1.875rem",
-                {
-                  lineHeight: "2.25rem",
-                  letterSpacing: "-0.02em",
-                  fontWeight: "700",
-                },
-              ],
-              h6: [
-                "1.875rem",
-                {
-                  lineHeight: "2.25rem",
-                  letterSpacing: "-0.02em",
-                  fontWeight: "700",
-                },
-              ],
-              body: [
-                "1.875rem",
-                {
-                  lineHeight: "2.25rem",
-                  letterSpacing: "-0.02em",
-                  fontWeight: "700",
-                },
-              ],
-              list: [
-                "1.875rem",
-                {
-                  lineHeight: "2.25rem",
-                  letterSpacing: "-0.02em",
-                  fontWeight: "700",
-                },
-              ],
-              link: [
-                "1.875rem",
-                {
-                  lineHeight: "2.25rem",
-                  letterSpacing: "-0.02em",
-                  fontWeight: "700",
-                },
-              ],
-              quote: [
-                "1.875rem",
-                {
-                  lineHeight: "2.25rem",
-                  letterSpacing: "-0.02em",
-                  fontWeight: "700",
-                },
-              ],
-              button: [
-                "1.875rem",
-                {
-                  lineHeight: "2.25rem",
-                  letterSpacing: "-0.02em",
-                  fontWeight: "700",
-                },
-              ],
-            },
-      extend: {},
+        ],
+        h2: [
+          "1.875rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+        h3: [
+          "1.875rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+        h4: [
+          "1.875rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+        h5: [
+          "1.875rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+        h6: [
+          "1.875rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+        body: [
+          "1.875rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+        list: [
+          "1.875rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+        link: [
+          "1.875rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+        quote: [
+          "1.875rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+        button: [
+          "1.875rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+      },
+      fontFamily: {
+        open: ["Open Sans", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      extend: {
+        aspectRatio: {
+          "hrect": "4 / 3",
+          "vrect": "3 / 4",
+        },
+      },
     },
     plugins: [],
-  } 
+  };
+  
