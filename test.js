6d1be3a7e81e0c5453c301841ae81eee16c4ed10
Yagnesh Modh <contact@thespecialcharacter.com>
2024-05-23 10:55:29 +0530

    commit code

---
+++ test.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+//             hoisting  redeclare   reassign   scope
+
+// var            yes       yes         yes      no
+
+// let            no        no          yes      yes
+
+// const          no        no          no       yes
