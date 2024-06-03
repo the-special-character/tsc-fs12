36492ca12a6e5eac5fe2e0a41030f7ad59edd4dd
Yagnesh Modh <contact@thespecialcharacter.com>
2024-06-03 10:16:38 +0530

    comit code

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var r={127:r=>{r.exports=(r,o)=>r+o}},o={};function t(e){var s=o[e];if(void 0!==s)return s.exports;var n=o[e]={exports:{}};return r[e](n,n.exports,t),n.exports}(()=>{const r=t(127)(1,2);console.log(r)})()})();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const add = require("./app");
+
+const a = 1;
+const b = 2;
+
+const ans = add(a, b);
+
+console.log(ans);
