03c29b762b2d6a0dee64ef82af7ec611e19535bb
Yagnesh Modh <contact@thespecialcharacter.com>
2024-06-05 10:21:39 +0530

    completed setup

--- webpack.config.js
+++ webpack.config.js
@@ -20,17 +20,12 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
+        use: ["babel-loader"],
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
       },
     ],
   },
