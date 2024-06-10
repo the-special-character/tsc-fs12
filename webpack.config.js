460566dfbd42da451eda8ceeca192d10811f8465
darshan <chotaliyadarshan710@gmail.com>
2024-06-10 13:48:07 +0530

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,32 @@ module.exports = {
     path: path.resolve(__dirname, 'output'),
     filename: './dist.main.js',
   },
+  mode : "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [' module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }']
+          }
+        }
+      }
+    ]
+  }
 };
