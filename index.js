0e5ee754dd610a9c84b1b944ed50cf362fb4e9a4
darshan <chotaliyadarshan710@gmail.com>
2024-06-10 17:47:16 +0530

    css

---
+++ index.js
@@ -0,0 +1,9 @@
+const add = require ("./app")
+require("./style.css")
+require ("./app")
+const  a =10;
+
+const b = 20;
+
+const ans = add (a,b)
+console.log(ans);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Darshan',
+          template: './public/index.html',
+          filename: 'index.html',
+          
+        }),
+      ],
+    output: {
+        path: path.resolve(__dirname, 'output'),
+        filename: 'main.js',
+      },
+     mode: "development",
+     module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+
+          }
+        ]
+      }
+  };
