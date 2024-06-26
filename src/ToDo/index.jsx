ff19f2d560d53ab5a6e9d0ece06a641b49805a61
Kush Vaishnav <kushvaishnav234@gmail.com>
2024-06-27 02:21:28 +0530

    added todo

--- src/ToDo/index.jsx
+++ src/ToDo/index.jsx
@@ -4,6 +4,8 @@ export default class ToDo extends Component {
   state = {
     todoList: [],
     todoText: "",
+    filterType: "all",
+    editedItem: [],
   };
   changeText = (event) => {
     this.setState({
@@ -33,7 +35,6 @@ export default class ToDo extends Component {
       }
     })
   }
-
   deleteTodo = (item) => {
     this.setState(({ todoList }, props) => {
       const index = todoList.findIndex((x) => x.id === item.id);
@@ -47,43 +48,146 @@ export default class ToDo extends Component {
   }
 
   render() {
-    const { todoList, todoText } = this.state;
+    const { todoList, todoText, filterType, editedItem } = this.state;
     return (
-      <div className='flex flex-col items-center h-screen overflow-auto'>
-        <h1 className=' text-4xl font-semibold my-6'>ToDo App</h1>
+      <div className='flex flex-col items-center h-screen overflow-auto bg-blue-100'>
+        <h1 className=' text-4xl font-semibold my-6 text-blue-500'>ToDo App</h1>
+
         {/* Form */}
         <form className='flex' onSubmit={this.addTodo}>
           <div>
             <label htmlFor="todoText" className='sr-only'>ToDo Text</label>
-            <input type="text" id="todoText" placeholder='Enter your todo here...' className='rounded-l-md' value={todoText} onChange={this.changeText} required />
+            <input
+              type="text"
+              id="todoText"
+              placeholder='Enter your todo here...' className='rounded-l-md'
+              value={todoText}
+              onChange={this.changeText}
+              required />
           </div>
-          <button type='submit' className="btn rounded-l-none"
+          <button
+            type='submit'
+            className="btn rounded-l-none"
           >Add Todo</button>
         </form>
+
         {/* List */}
         <div className='flex flex-1 flex-col gap-4 w-full p-6 overflow-auto'>
           {todoList.map((item) => {
-            return (
-              <div className='flex items-center justify-center p-2 bg-cyan-200 hover:bg-fuchsia-200' key={item.id}>
-                <div>
-                  <label htmlFor="isCompleted" className='sr-only'>Toggle Complete</label>
-                  <input type="checkbox" id="isCompleted" checked={item.isDone}
-                    onChange={() => this.updateTodo(item)} />
-                </div>
-                <p className='px-6 flex-1 line-clamp-2'>{item.todoText}</p>
-                <button type='submit' className="btn" onClick={() => this.deleteTodo(item)}
-                >Delete</button>
-              </div>
-            );
+            if (
+              filterType === "all" ||
+              filterType === "Pending" && item.isDone === false ||
+              filterType === "Completed" && item.isDone === true) {
+              return (
+                <div className='flex gap-4 items-center justify-center p-2' key={item.id}>
+                  <div>
+                    <label htmlFor="isCompleted" className='sr-only'>Toggle Complete</label>
+                    <input
+                      type="checkbox"
+                      id="isCompleted"
+                      checked={item.isDone}
+                      onChange={() => this.updateTodo(item)} />
+                  </div>
+                  {
+                    editedItem.some(x => x.id === item.id) ? <form className='flex flex-1'>
+                      <div className='flex-1'>
+                        <label htmlFor="editTodo" className='sr-only'>Edit Todo</label>
+                        <input
+                          type="text"
+                          id="editTodo"
+                          value={item.todoText}
+                          onChange={(event) => {
+                            this.setState(({ todoList }) => {
+                              const index = todoList.findIndex((x) => x.id === item.id);
+                              return {
+                                todoList: [
+                                  ...todoList.slice(0, index),
+                                  {
+                                    ...todoList[index],
+                                    todoText: event.target.value,
+                                  },
+                                  ...todoList.slice(index + 1),
+                                ],
+                              }
+                            })
+                          }} />
+                      </div>
+                      <button
+                        type='submit'
+                        className='btn'
+                        onClick={(event) => {
+                          event.preventDefault();
+                          this.setState(({ editedItem }, props) => {
+                            const index = todoList.findIndex((x) => x.id === item.id);
+                            return {
+                              editedItem: [
+                                [
+                                  ...editedItem.slice(0, index),
+                                  ...editedItem.slice(index + 1)
+                                ]
+                              ]
+                            }
+                          })
+                        }}>
+                        Update Value
+                      </button>
+                    </form> :
+                      <p className='flex-1 line-clamp-2'>{item.todoText}</p>
 
+                  }
+                  <button
+                    type='submit'
+                    className="btn"
+                    onClick={() => {
+                      this.setState(({ editedItem }, props) => {
+                        return {
+                          editedItem: [...editedItem, item]
+                        }
+                      })
+                    }}
+                  >Edit</button>
+                  <button
+                    type='submit'
+                    className="btn"
+                    onClick={() => this.deleteTodo(item)}
+                  >Delete</button>
+                </div>
+              );
+            }
+            return null;
           })}
 
         </div>
+
         {/* Filter type */}
-        <div className='w-full flex '>
-          <button type='button' className='btn rounded-none flex-1'>All</button>
-          <button type='button' className='btn rounded-none flex-1'>Pending</button>
-          <button type='button' className='btn rounded-none flex-1'>Completed</button>
+        <div className='w-full flex'>
+          <button
+            type='button'
+            className='btn rounded-none flex-1'
+            style={
+              { backgroundColor: filterType === 'all' ? 'green' : 'blue' }
+            }
+            onClick={() => {
+              this.setState({ filterType: 'all' })
+            }}>All</button>
+          <button
+            type='button'
+            className='btn rounded-none flex-1'
+            style={
+              { backgroundColor: filterType === 'Pending' ? 'green' : 'blue' }
+            }
+            onClick={() => {
+              this.setState({ filterType: 'Pending' })
+            }}>Pending</button>
+          <button
+            type='button'
+            className='btn rounded-none flex-1'
+            style={
+              { backgroundColor: filterType === 'Completed' ? 'green' : 'blue' }
+            }
+            onClick={() => {
+              this.setState({ filterType: 'Completed' })
+            }}>Completed</button>
         </div>
       </div>
     )
