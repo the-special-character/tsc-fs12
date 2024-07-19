c8282bc6a2746ab317ba4d958b27c77bf85046e0
Kush Vaishnav <kushvaishnav234@gmail.com>
2024-07-20 00:10:20 +0530

    add

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { createRoot } from "react-dom/client";
 import "./style.css";
-import LifeCycle from "./LifeCycle";
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
@@ -9,41 +8,5 @@ document.body.innerHTML = '<div id="app"></div>';
 // Render your React component instead
 const root = createRoot(document.getElementById("app"));
 
-const test = "hello";
-LifeCycle.getDerivedStateFromProps = (props, state) => {
-    // console.log("getDerivedStateFromProps Life Cycle");
-
-    console.log(props, state);
-    return {
-        name: `Mr. ${props.name}`,
-        test,
-    };
-}
-
-export default class App extends Component {
-    state = {
-        name: "Kush R Vaishnav",
-    }
-
-    static getDerivedStateFromError(error) {
-        return {
-            error,
-        }
-    }
-    componentDidCatch(error, errorinfo) {
-        console.log(errorinfo.componentStack);
-
-    }
-
-    render() {
-        const { name, error } = this.state;
-        return <div>
-            {error ? <p>{error.message}</p> : <LifeCycle name={name} />}
-            <button type='button' className='btn' onClick={() => {
-                this.setState({ name: "Virat Kohli" })
-            }}>Change Name</button>
-        </div>
-    }
-}
 
 root.render(<App />)  
